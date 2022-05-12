a03e5f7c25323e168d78113eed434793c84b3dde
Petar Tushev <tushevio@hotmail.com>
2022-05-12 10:54:40 +0200

    Prediction script changes.

--- ML script/app.py
+++ ML script/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, jsonify
+import joblib
 
 app = Flask(__name__)
 
 
 @app.route('/predict', methods=['POST'])
-def predict():
+def predict(comment):
+
     json = request.json()
-    return jsonify({'result':  list()})
+    d = { 'NEGATIVE': 0 , 'POSITIVE': 1 }
+    
+    model = joblib.load( 'model.pkl' )
+    pred = model.predict( comment )
+    result = d[ pred[ 'label' ] ]
+
+    return jsonify({'result':  result})
 
 
 if __name__ == '__main__':
